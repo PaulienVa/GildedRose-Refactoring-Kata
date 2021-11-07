13865e43f945560c893c4c8be1fc802956c0d1eb
Paulien van Alst <coding@paulienvanalst.com>
2021-11-07 20:18:29 +0100

    Use enhanced list to increase readability

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,57 +7,55 @@ class GildedRose(var items: Array<Item>) {
     private val sulfuras = "Sulfuras, Hand of Ragnaros"
 
     private val maximumQuality = 50
-
     private val minimalNormalSellInBackStage = 11
-
     private val minimalHighSellInBackStage = 6
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != agedBrie && items[i].name != backStagePasses) {
-                if (items[i].quality > 0) {
-                    if (items[i].name != sulfuras) {
-                        items[i].quality = items[i].quality - 1
+        for (item in items) {
+            if (item.name != agedBrie && item.name != backStagePasses) {
+                if (item.quality > 0) {
+                    if (item.name != sulfuras) {
+                        item.quality = item.quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < maximumQuality) {
-                    items[i].quality = items[i].quality + 1
+                if (item.quality < maximumQuality) {
+                    item.quality = item.quality + 1
 
-                    if (items[i].name == backStagePasses) {
-                        if (items[i].sellIn < minimalNormalSellInBackStage) {
-                            if (items[i].quality < maximumQuality) {
-                                items[i].quality = items[i].quality + 1
+                    if (item.name == backStagePasses) {
+                        if (item.sellIn < minimalNormalSellInBackStage) {
+                            if (item.quality < maximumQuality) {
+                                item.quality = item.quality + 1
                             }
                         }
 
-                        if (items[i].sellIn < minimalHighSellInBackStage) {
-                            if (items[i].quality < maximumQuality) {
-                                items[i].quality = items[i].quality + 1
+                        if (item.sellIn < minimalHighSellInBackStage) {
+                            if (item.quality < maximumQuality) {
+                                item.quality = item.quality + 1
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != sulfuras) {
-                items[i].sellIn = items[i].sellIn - 1
+            if (item.name != sulfuras) {
+                item.sellIn = item.sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != agedBrie) {
-                    if (items[i].name != backStagePasses) {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != sulfuras) {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.name != agedBrie) {
+                    if (item.name != backStagePasses) {
+                        if (item.quality > 0) {
+                            if (item.name != sulfuras) {
+                                item.quality = item.quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = item.quality - item.quality
                     }
                 } else {
-                    if (items[i].quality < maximumQuality) {
-                        items[i].quality = items[i].quality + 1
+                    if (item.quality < maximumQuality) {
+                        item.quality = item.quality + 1
                     }
                 }
             }
