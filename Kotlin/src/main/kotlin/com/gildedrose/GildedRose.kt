31153fd4d94d9d14003cfa5ab5bdddc45e3726dd
Paulien van Alst <coding@paulienvanalst.com>
2021-11-07 20:16:10 +0100

    Introduce properties for repeating numbers to avoid magical numbers (other than 0 or 1 for now)

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,6 +6,12 @@ class GildedRose(var items: Array<Item>) {
     private val backStagePasses = "Backstage passes to a TAFKAL80ETC concert"
     private val sulfuras = "Sulfuras, Hand of Ragnaros"
 
+    private val maximumQuality = 50
+
+    private val minimalNormalSellInBackStage = 11
+
+    private val minimalHighSellInBackStage = 6
+
     fun updateQuality() {
         for (i in items.indices) {
             if (items[i].name != agedBrie && items[i].name != backStagePasses) {
@@ -15,18 +21,18 @@ class GildedRose(var items: Array<Item>) {
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (items[i].quality < maximumQuality) {
                     items[i].quality = items[i].quality + 1
 
                     if (items[i].name == backStagePasses) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < minimalNormalSellInBackStage) {
+                            if (items[i].quality < maximumQuality) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < minimalHighSellInBackStage) {
+                            if (items[i].quality < maximumQuality) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
@@ -50,7 +56,7 @@ class GildedRose(var items: Array<Item>) {
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (items[i].quality < maximumQuality) {
                         items[i].quality = items[i].quality + 1
                     }
                 }
