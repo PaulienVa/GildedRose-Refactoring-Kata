bbc9e98e78b1710dffbfc2c81572b7e972aeb5f3
Paulien van Alst <coding@paulienvanalst.com>
2021-11-07 20:36:33 +0100

    Substracting quality from the same quality is equivalent to setting it to  0 and more readable

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -41,7 +41,7 @@ class GildedRose(var items: Array<Item>) {
                     if (item.name != backStagePasses) {
                         item.quality = decreaseQuality(item)
                     } else {
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                     }
                 } else {
                     if (item.quality < maximumQuality) {
